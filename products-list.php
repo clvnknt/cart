18d75e2f37957784a6fac89d5c26aa63be4e8fc0
clvnknt <calvinkent.p@gmail.com>
2022-03-22 14:56:02 +0800

    Customized the design, added my own logo, updated the layout of products, and shopping

--- products-list.php
+++ products-list.php
@@ -9,35 +9,59 @@ $products = Product::convertArrayToProducts($products_data);
 
 $customer = new Customer('John Doe', 'john@mail.com');
 ?>
+
 <html>
 <head>
     <title>My Cart</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 </head>
-<body>
+<body style="background-image: url('wallpaper.png');">
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+  <img src="FruitiliciousLOGO.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+    <a class="navbar-brand" href="#">Fruitilicious</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="shopping-cart.php">Shopping Cart</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
 <h1>Welcome <?php echo $customer->getName() ?>!</h1>
+<div class="mx-auto" style="width: 200px;">
 <h2>Products</h2>
-<h4>
-    <a href="shopping-cart.php">Shopping Cart</a>
-</h4>
 
 <?php foreach ($products as $product): ?>
 
 <form action="add-to-cart.php" method="POST">
     <input type="hidden" name="product_id" value="<?php echo $product->getId(); ?>" />
+
     <h3><?php echo $product->getName(); ?></h3>
     <img src="<?php echo $product->getImage(); ?>" height="100px" />
     <p>
         <?php echo $product->getDescription(); ?><br/>
         <span style="color: blue">PHP <?php echo $product->getPrice(); ?></span>
         Qty <input type="number" name="quantity" class="quantity" value="0" />
-        <button type="submit">
+        <br><br><button type="submit" class="btn btn-primary"> 
             ADD TO CART
         </button>
     </p>
 </form>
 
 <?php endforeach; ?>
-
+</div>
+</div>
 </body>
 </html>
--- shopping-cart.php
+++ shopping-cart.php
@@ -16,10 +16,32 @@ $shoppingCartItems = $shoppingCart->getAllItems();
 <html>
 <head>
     <title>My Cart</title>
-</head>
-<body>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+<body style="background-image: url('wallpaper.png');">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+  <img src="FruitiliciousLOGO.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+    <a class="navbar-brand" href="#">Fruitilicious</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="products-list.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="shopping-cart.php">Shopping Cart</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 <h1>Welcome <?php echo $customer->getName() ?>!</h1>
+<div class="mx-auto" style="width: 200px;">
 <h2>Shopping Cart</h2>
 <h4>
     <a href="products-list.php">Shop More Products</a>
